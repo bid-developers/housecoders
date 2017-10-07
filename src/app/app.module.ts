4755d00ea8001f152a62bcc4174671ad38905131
Letitia Shaanika <letishaanika@gmail.com>
2017-10-07 10:52:56 +0200

    Letitia Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ChartModule } from 'angular2-highcharts';
 import * as highcharts from 'Highcharts';
+import { FirebaseProvider } from '../providers/firebase/firebase';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireModule}from 'angularfire2';
+import{AngularFireAuthModule} from 'angularfire2/auth';
 
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyASvO9CFmFVlQdZahOCAsmectq0ygO8NXU",
+    authDomain: "housecoders-d9cf8.firebaseapp.com",
+    databaseURL: "https://housecoders-d9cf8.firebaseio.com",
+    projectId: "housecoders-d9cf8",
+    storageBucket: "housecoders-d9cf8.appspot.com",
+    messagingSenderId: "1035526976218"
+  };
+  
 @NgModule({
   declarations: [
     MyApp,
     HomePage,FundmePage,RedeemPage,ShopPage,TabsPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,AngularFireModule.initializeApp(config),AngularFireDatabaseModule,AngularFireAuthModule,
     IonicModule.forRoot(MyApp),ChartModule.forRoot(highcharts)
   ],
   bootstrap: [IonicApp],
@@ -32,7 +46,8 @@ import * as highcharts from 'Highcharts';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
