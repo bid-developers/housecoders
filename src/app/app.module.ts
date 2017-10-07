031dc71da10c5cd512f34146b719af9ecaf54186
leslie <lmavonyani@gmail.com>
2017-10-07 02:16:13 +0200

    Leslie Added Charts to first Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ChartModule } from 'angular2-highcharts';
+import * as highcharts from 'Highcharts';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp),ChartModule.forRoot(highcharts)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as HighCharts from 'highcharts';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
+//@Input()
+        //chartOptions:any;
+  constructor(public navCtrl: NavController) {}
+      /*this.chartOptions = {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Fruit Consumption'
+    },
+    xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit eaten'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+    }, {
+        name: 'John',
+        data: [5, 7, 3]
+    }]
+}
+  }*/
+ionViewDidLoad(){
+    var myChart = HighCharts.chart('container', {
 
-  constructor(public navCtrl: NavController) {
+        chart: {
 
-  }
+            type: 'bar'
+
+        },
+
+        title: {
+
+            text: 'Fruit Consumption'
+
+        },
+
+        xAxis: {
+
+            categories: ['Apples', 'Bananas', 'Oranges']
+
+        },
+
+        yAxis: {
+
+            title: {
+
+                text: 'Fruit eaten'
+
+            }
+
+        },
+
+        series: [{
+
+            name: 'Jane',
+
+            data: [1, 0, 4]
+
+        }, {
+
+            name: 'John',
+
+            data: [5, 7, 3]
+
+        }]
+
+    });
+}
 
 }
