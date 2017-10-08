78120e2ead08d620fea694b5c3ba44c909325909
Clerence pc <kameetac@gmail.com>
2017-10-08 19:46:59 +0200

    shop features

--- src/pages/monetary/monetary.ts
+++ src/pages/monetary/monetary.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController,AlertController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, AlertController, NavParams} from 'ionic-angular';
 import 'rxjs/add/operator/map';
 //import {FirebaseDetail} from '../firebase-detail/firebase-detail'
-import { FirebaseListObservable,
-    FirebaseObjectObservable,
-    AngularFireDatabase
-} from 'angularfire2/database';
+import {FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase} from 'angularfire2/database';
 import firebase from 'firebase';
-import { LoadingController } from 'ionic-angular';
-
+import {LoadingController} from 'ionic-angular';
+import {CardIO} from '@ionic-native/card-io';
 /**
  * Generated class for the MonetaryPage page.
  *
@@ -16,20 +13,16 @@ import { LoadingController } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@Component({
-  selector: 'page-monetary',
-  templateUrl: 'monetary.html'
-})
+@Component({selector: 'page-monetary', templateUrl: 'monetary.html'})
 export class MonetaryPage {
-    items:FirebaseListObservable<any[]>;
-    item:FirebaseObjectObservable<any>;
-    contrib_detail: FirebaseListObservable <any[]>;
-    currency:any;
-    amount:any;
-    timestamp: any;
+    items : FirebaseListObservable < any[] >;
+    item : FirebaseObjectObservable < any >;
+    contrib_detail : FirebaseListObservable < any[] >;
+    currency : any;
+    amount : any;
+    timestamp : any;
 
-
-    constructor(db:AngularFireDatabase, public nav:NavController, private alertCtrl:AlertController,public loadingCtrl: LoadingController) {
+    constructor(db : AngularFireDatabase, public nav : NavController, private alertCtrl : AlertController, public loadingCtrl : LoadingController, private cardIO : CardIO, public navParams : NavParams) {
         this.contrib_detail = db.list('/contrib_detail', {
             query: {
                 orderByChild: 'score'
@@ -42,60 +35,98 @@ export class MonetaryPage {
 
     bluewallet(currency, amount) {
 
+        let prompt = this
+            .alertCtrl
+            .create({
+                title: 'Are you sure you want to proceed',
+                message: "You are about to pay " + currency + " " + amount + " to the buy a brick Campaign using your blue wallet",
+                inputs: [
+                    {
+                        name: 'displayname',
+                        placeholder: 'Enter Blue Wallet PIN'
+                    }
 
-
-     let prompt = this.alertCtrl.create({
-     title: 'Are you sure you want to proceed',
-     message: "You are about to pay "+currency+" "+ amount + " to the buy a brick Campaign using your blue wallet",
-     inputs: [
-     {
-     name: 'displayname',
-     placeholder: 'Enter Blue Wallet PIN'
-     }
-
-     ],
-     buttons: [
-     {
-     text: 'Cancel',
-     handler: data => {
-     console.log('Cancel clicked');
-     }
-     },
-         {
-             text: 'Confirm',
-             handler: data => {
-                 console.log('Saved clicked');
-                 window.localStorage.setItem('amiProfilename', data.displayname);
-                 this.contrib_detail.push({ contribution: "Monetary", contribution_name: currency, timestamp: this.timestamp, measure: amount })
-                 this.presentLoading();
-             }
-         }
-          ]
-     });
+                ],
+                buttons: [
+                    {
+                        text: 'Cancel',
+                        handler: data => {
+                            console.log('Cancel clicked');
+                        }
+                    }, {
+                        text: 'Confirm',
+                        handler: data => {
+                            console.log('Saved clicked');
+                            window
+                                .localStorage
+                                .setItem('amiProfilename', data.displayname);
+                            this
+                                .contrib_detail
+                                .push({contribution: "Monetary", contribution_name: currency, timestamp: this.timestamp, measure: amount})
+                            this.presentLoading();
+                        }
+                    }
+                ]
+            });
         prompt.present();
 
     }
 
+    scan(currency,amount){
+        this.cardIO.canScan().then(
+          (res: boolean) => {
+            if(res){
+              let options = {
+                requireCardholderName: true,
+                requireExpiry: true,
+                requireCVV: true,
+                requirePostalCode: false,
+                scanInstructions: "Scan the front of your card",
+                scanExpiry: true,
+                scanCardHolderName: true,
+                guideColor: '#12be76',
+                hideCardIOLogo: false,
+                suppressConfirmation: false
+              };
+              this.cardIO.scan(options).then((res:any)=>{
+               this.showAlert(res['cardNumber']);
+              });
+            }
+          });
+        }
 
+
+        showAlert(text:string) {
+            let alert = this.alertCtrl.create({
+              title: 'alert',
+              subTitle: text,
+              buttons: ['OK']
+            });
+            alert.present();
+          }
 
     success() {
 
-        let alert = this.alertCtrl.create({
-            title: 'Payment Success',
-            subTitle: 'Thank you for your contribution to the buy a brick campaign, it will go a long way to building a house for someone who really needs it!',
-            buttons: ['OK']
-        });
+        let alert = this
+            .alertCtrl
+            .create({
+                title: 'Payment Success',
+                subTitle: 'Thank you for your contribution to the buy a brick campaign, it will go a long w' +
+                        'ay to building a house for someone who really needs it!',
+                buttons: ['OK']
+            });
 
         alert.present();
     }
 
     presentLoading() {
-        let loader = this.loadingCtrl.create({
-            content: "Payment in progress, please wait...",
-            duration: 1500
-        });this.success();
+        let loader = this
+            .loadingCtrl
+            .create({content: "Payment in progress, please wait...", duration: 1500});
+        this.success();
 
         loader.present();
 
     }
+   
 }
--- src/pages/shop/shop.ts
+++ src/pages/shop/shop.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { CardIO } from '@ionic-native/card-io';
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+import {FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase} from 'angularfire2/database';
+import {LoadingController} from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-shop',
@@ -9,16 +13,77 @@ import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-sca
 })
 export class ShopPage {
 
+  items : FirebaseListObservable < any[] >;
+  item : FirebaseObjectObservable < any >;
+  contrib_detail : FirebaseListObservable < any[] >;
+  currency : any;
+  amount : any;
+  timestamp : any;
+
   scanCodeData: {};
   options: BarcodeScannerOptions;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public loadingCtrl : LoadingController,
+             db : AngularFireDatabase,
+             public navCtrl: NavController,
              public navParams: NavParams,
              private cardIO:CardIO,
              private barcodeScanner: BarcodeScanner,
              public alertCtrl: AlertController
             ) {
+
+              this.contrib_detail = db.list('/contrib_detail', {
+                query: {
+                    orderByChild: 'score'
+                }
+            })
+    
+            this.timestamp = firebase.database.ServerValue.TIMESTAMP;
+            //       console.log(this.items)
+
+
   }
+
+
+  bluewallet(currency, amount) {
+    
+            let prompt = this
+                .alertCtrl
+                .create({
+                    title: 'Are you sure you want to proceed',
+                    message: "You are about to pay " + currency + " " + amount + " to the buy a brick Campaign using your blue wallet",
+                    inputs: [
+                        {
+                            name: 'displayname',
+                            placeholder: 'Enter Blue Wallet PIN'
+                        }
+    
+                    ],
+                    buttons: [
+                        {
+                            text: 'Cancel',
+                            handler: data => {
+                                console.log('Cancel clicked');
+                            }
+                        }, {
+                            text: 'Confirm',
+                            handler: data => {
+                                console.log('Saved clicked');
+                                window
+                                    .localStorage
+                                    .setItem('amiProfilename', data.displayname);
+                                this
+                                    .contrib_detail
+                                    .push({contribution: "Monetary", contribution_name: currency, timestamp: this.timestamp, measure: amount})
+                                this.presentLoading();
+                            }
+                        }
+                    ]
+                });
+            prompt.present();
+    
+        }
 
   Qrscan(){
     this.options = {
@@ -42,14 +107,14 @@ scan(){
         let options = {
           requireCardholderName: true,
           requireExpiry: true,
-          requireCCV: true,
+          requireCVV: true,
           requirePostalCode: false,
           scanInstructions: "Scan the front of your card",
           scanExpiry: true,
-          scanCVV: true,
           scanCardHolderName: true,
           guideColor: '#12be76',
-          hideCardIOLogo: true
+          hideCardIOLogo: false,
+          suppressConfirmation: false
         };
         this.cardIO.scan(options).then((res:any)=>{
          this.showAlert(res['cardNumber']);
@@ -62,12 +127,40 @@ scan(){
     console.log('ionViewDidLoad ShopPage');
   }
 
+
+
   showAlert(text:string) {
     let alert = this.alertCtrl.create({
-      title: 'alert',
-      subTitle: text,
+      title: 'Payment Success',
+      subTitle: 'Thank you for your contribution to the buy a brick campaign, it will go a long w' +
+              'ay to building a house for someone who really needs it!',
       buttons: ['OK']
     });
     alert.present();
   }
+
+  success() {
+    
+            let alert = this
+                .alertCtrl
+                .create({
+                    title: 'Payment Success',
+                    subTitle: 'Thank you for your contribution to the buy a brick campaign, it will go a long w' +
+                            'ay to building a house for someone who really needs it!',
+                    buttons: ['OK']
+                });
+    
+            alert.present();
+        }
+
+  presentLoading() {
+    let loader = this
+        .loadingCtrl
+        .create({content: "Payment in progress, please wait...", duration: 1500});
+    this.success();
+
+    loader.present();
+
+}
+
 }
